54a53fdfa3e5526789cf5a8476a74baf909d62fd
nandahn <mariafboliveira2300@gmail.com>
2024-09-03 19:20:52 -0300

    exercicio de verificação de numeros

---
+++ Exercicios/verificacao-de-numero/src/VerificarNumeros.java
@@ -0,0 +1,15 @@
+public class VerificarNumeros {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        if (numero > 0) {
+            System.out.println("O numero é positivo");
+            
+        } else if (numero < 0) {
+            System.out.println("O numero é negativo");
+            
+        } else {
+            System.out.println("O numero é zero");
+        }
+    }
+}
