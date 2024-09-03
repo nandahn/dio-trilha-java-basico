d09f05f464b5a7ca3683d281867fe60b5de2feae
nandahn <mariafboliveira2300@gmail.com>
2024-09-03 15:20:28 -0300

    exercicio de calculo de média de alunos

---
+++ Exercicios/sistema-escolar/src/MediaEscolar.java
@@ -0,0 +1,17 @@
+public class MediaEscolar {
+    public static void main(String[] args) {
+        String aluno = "Maria";
+        double n1 = 6.0;
+        double n2 = 6.0;
+        double mediaFinal = (n1 + n2) / 2;
+
+        if (mediaFinal >= 6.0) {
+
+            System.out.println("Aprovado com média: " + mediaFinal);
+        } else {
+            System.out.println("Reprovado com média: " + mediaFinal);
+        }
+
+
+    }
+}
