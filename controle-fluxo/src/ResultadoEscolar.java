7b477481491ff35f25effefe9490dbd1a09561a5
nandahn <mariafboliveira2300@gmail.com>
2024-09-04 19:05:03 -0300

    inserindo condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -4,10 +4,15 @@ public class ResultadoEscolar {
         int nota = 5;
 
         if (nota >= 7) {
-            System.out.println("Aprovado!!!");          
-        }else{
+            System.out.println("Aprovado!!!");  
+            
+        }else if (nota >= 5 && nota < 7) 
+            System.out.println("Prova de Recuperação!!!");  
+            
+        
+        else
             System.out.println("Reprovado!!");
-        }
+        
 
     }
 }
