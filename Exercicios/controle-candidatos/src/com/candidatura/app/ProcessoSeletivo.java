8ac0bdcbb65dfb0e6b94a79adc3cedcb5ecface3
nandahn <mariafboliveira2300@gmail.com>
2024-09-17 19:24:24 -0300

    Selecionando candidatos

--- Exercicios/controle-candidatos/src/com/candidatura/app/ProcessoSeletivo.java
+++ Exercicios/controle-candidatos/src/com/candidatura/app/ProcessoSeletivo.java
@@ -1,5 +1,50 @@
 package com.candidatura.app;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE",  "MARIA", "MARCIA", "JULIA", 
+        "FERNANDA", "RAFAEL", "JOÃO" , "GUILHERME" , "ANTÔNIO" , "MIRELA"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato: " + candidato + 
+            "solicitou este valor de salário: " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato: " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;                
+            }
+            candidatosAtual++;
+            
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato (double salarioPretendido){
+        
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+            
+        } else if (salarioBase==salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
     
 }
